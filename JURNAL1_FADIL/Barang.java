4cb598c0bf0cf8768a61b7e3c546f325bfef9b45
Fadilah170 <m.fadhilah333@gmail.com>
2024-10-14 08:26:56 +0700

    JURNAL1

---
+++ JURNAL1_FADIL/Barang.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Barang {
+    //Atribut
+    private String nama;
+    private int jumlah;
+    private double harga;
+        
+    // Constructor Barang
+    public Barang (String nama,int jumlah,double harga){
+        this.nama = nama;
+        this.jumlah = jumlah;
+        this.harga = harga;
+    }
+
+        // Getter dan Setter
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+
+    
+
+
+    // Method tampilkanData
+    public void tampilkanData(){
+        System.out.println("nama Barang " +nama);
+        System.out.println("jumlah"+ jumlah);
+        System.out.println("Harga" +harga);
+
+        
+    }
+
+}
---
+++ JURNAL1_FADIL/ManajemenInventaris.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ManajemenInventaris {
+    // Deklarasi ArrayList untuk menyimpan barang
+    public static void main(String[] ) {
+        ArrayList<Barang> daftarbarang = new ArrayList<>();
+
+
+    // Method untuk menambah barang elektronik
+
+        // Scanner 
+
+        // Input Nama Barang, Jumlah Barang, Harga Barang, Garansi Barang
+
+        // Membuat objek barang elektronik baru
+
+
+    // Method untuk menambah barang non-elektronik
+
+        // Scanner
+
+
+        // Input Nama Barang, Jumlah Barang, Harga Barang, Material Barang
+
+        // Membuat objek barang non-elektronik baru
+
+
+    // Method untuk menampilkan semua barang memakai if-else (jika tidak ada barang akan menampilkan tidak ada barang dalam inventaris)
+
+}
